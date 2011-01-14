29bb17becefd90d5fcd8fa6fff69a7694a03fa64
dow <dave.walton@jax.org>
2011-01-14 13:30:08 -0500

    Added scruffy test cases

--- modules/drake-genetics-client/src/java/org/jax/drakegenetics/gwtclientapp/client/DrakeSetGenerator.java
+++ modules/drake-genetics-client/src/java/org/jax/drakegenetics/gwtclientapp/client/DrakeSetGenerator.java
@@ -18,9 +18,11 @@
 package org.jax.drakegenetics.gwtclientapp.client;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import org.jax.drakegenetics.shareddata.client.Chromosome;
 import org.jax.drakegenetics.shareddata.client.DiploidGenome;
 import org.jax.drakegenetics.shareddata.client.DrakeSpeciesSingleton;
 
@@ -39,6 +41,9 @@ public class DrakeSetGenerator  {
     private Map<String, String> malePhenome = new HashMap<String,String>();
     private Map<String, String> sexRevPhenome = new HashMap<String,String>();
     private Map<String, String> diabeticPhenome = new HashMap<String,String>();
+    private Map<String, String> scruffyFemalePhenome = new HashMap<String,String>();
+    private Map<String, String> scruffyMalePhenome = new HashMap<String,String>();
+
 
     private void setFemalePhenome(Map<String,String> phenome) {
         Set<String> keys = phenome.keySet();
@@ -68,7 +73,21 @@ public class DrakeSetGenerator  {
         }
     }
     
-    public Folder getTreeModel(DrakeGeneticsServiceAsync dgs) {
+    private void setScruffyFemalePhenome(Map<String,String> phenome) {
+        Set<String> keys = phenome.keySet();
+        for (String key: keys) {
+            scruffyFemalePhenome.put(key,phenome.get(key));
+        }
+    }
+    
+    private void setScruffyMalePhenome(Map<String,String> phenome) {
+        Set<String> keys = phenome.keySet();
+        for (String key: keys) {
+            scruffyMalePhenome.put(key,phenome.get(key));
+        }
+    }
+    
+   public Folder getTreeModel(DrakeGeneticsServiceAsync dgs) {
         Image f_small_example = new Image("images/eyes/SEF51311.jpg");
         Image f_large_example = new Image("images/eyes/LEF51311.jpg");
         Image m_small_example = new Image("images/eyes/SEM40100.jpg");
@@ -78,15 +97,48 @@ public class DrakeSetGenerator  {
         Image b_small_example = new Image("images/eyes/SEM40100.jpg");
         Image b_large_example = new Image("images/eyes/LEM40100.jpg");
         
+        // Create our set of Genotypes
+        // normal female
         DiploidGenome female_genome = new DiploidGenome("P1_M", "P1_P", true,
                 DrakeSpeciesSingleton.getInstance());
+        // normal male
         DiploidGenome male_genome = new DiploidGenome("P2_M", "P2_P", false,
                 DrakeSpeciesSingleton.getInstance());
+        // sex reversed male
         DiploidGenome sex_rev_male_genome = new DiploidGenome("PAT_M", "P2_P", 
                 false, DrakeSpeciesSingleton.getInstance());
+        // diabetic female
         DiploidGenome db_female_genome = new DiploidGenome("DB_M", "DB_P", 
                 true, DrakeSpeciesSingleton.getInstance());
+        // scruffy female
+        DiploidGenome sc_female_genome = new DiploidGenome("P1_M", "P1_P", true,
+                DrakeSpeciesSingleton.getInstance());
+        List<Chromosome> maternalHaploid = sc_female_genome.getMaternalHaploid();
+        Chromosome toRemove = new Chromosome();
+        for (Chromosome chr : maternalHaploid) {
+            if (chr.getChromosomeName().equals("X")) {
+                   toRemove = chr;
+                   // only remove one
+                   break;
+                }
+        }
+        maternalHaploid.remove(toRemove);
+        sc_female_genome.setPaternalHaploid(maternalHaploid);
+        // scruffy male
+        DiploidGenome sc_male_genome = new DiploidGenome("P2_M", "P2_P", false,
+                DrakeSpeciesSingleton.getInstance());
+        maternalHaploid = sc_male_genome.getMaternalHaploid();
+        Chromosome toAdd = new Chromosome();
+        for (Chromosome chr : maternalHaploid) {
+            if (chr.getChromosomeName().equals("X")) {
+                   //maternalHaploid.add(new Chromosome(chr));
+                toAdd = new Chromosome(chr);
+                }
+        }
+        maternalHaploid.add(toAdd);
+        sc_male_genome.setMaternalHaploid(maternalHaploid);
 
+        // Get all the phenotypes
         dgs.getPhenome(female_genome,
                 new AsyncCallback<Map<String, String>>() {
                     public void onSuccess(Map<String, String> phenome) {
@@ -143,6 +195,34 @@ public class DrakeSetGenerator  {
                     }
                 });
         
+        dgs.getPhenome(sc_female_genome,
+                new AsyncCallback<Map<String, String>>() {
+                    public void onSuccess(Map<String, String> phenome) {
+                        GWT.log("Have Scruffy Female Phenotype!");
+                        GWT.log(phenome.toString());
+                        setScruffyFemalePhenome(phenome);
+                    }
+
+                    public void onFailure(Throwable caught) {
+                        caught.printStackTrace();
+                        GWT.log(caught.getMessage());
+                    }
+                });
+
+        dgs.getPhenome(sc_male_genome,
+                new AsyncCallback<Map<String, String>>() {
+                    public void onSuccess(Map<String, String> phenome) {
+                        GWT.log("Have Scruffy Male Phenotype!");
+                        GWT.log(phenome.toString());
+                        setScruffyMalePhenome(phenome);
+                    }
+
+                    public void onFailure(Throwable caught) {
+                        caught.printStackTrace();
+                        GWT.log(caught.getMessage());
+                    }
+                });
+        
         Folder[] folders = new Folder[] {
                 new Folder("Females", new Drake[] { new Drake("P1",
                         female_genome, femalePhenome,
@@ -150,10 +230,14 @@ public class DrakeSetGenerator  {
                         new Drake("PAT", sex_rev_male_genome, sexRevPhenome,
                                 b_small_example, b_large_example),
                         new Drake("DB", db_female_genome, diabeticPhenome,
-                                        d_small_example, d_large_example),}),
+                                        d_small_example, d_large_example),
+                        new Drake("Scooby", sc_female_genome, scruffyFemalePhenome,
+                                                f_small_example, f_large_example),}),
                 new Folder("Males", new Drake[] { new Drake("P2",
                         male_genome, malePhenome,
-                        m_small_example, m_large_example), }) };
+                        m_small_example, m_large_example), 
+                        new Drake("Shaggy", sc_male_genome, scruffyMalePhenome,
+                                m_small_example, m_large_example),}) };
 
         Folder root = new Folder("root");
         for (int i = 0; i < folders.length; i++) {
